0f5c8db2f8c550fbcd6f48fa7f5db0603a85adb2
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2024-09-19 22:35:28 -0300

    Update script.js

--- atividadeCase/script.js
+++ atividadeCase/script.js
@@ -1,28 +1,28 @@
-function calcularSalario() {
-    // Obtém valores do formulário
-    let salario = parseFloat(document.getElementById('salario').value);
-    let cargo = document.getElementById('cargo').value;
-    let percentual = 0;
-    let salarioFinal = 0;
+var botao = document.getElementById("botao");
 
-    // Controle de ações usando switch-case
-    switch (cargo) {
+botao.onclick = function(){
+    let salario = parseFloat(document.getElementById("salario").value);
+    let cargo = document.getElementById("cargo").value;
+    let resultado = document.getElementById("resultado");
+    let salarioFinal = 0;
+    let percentual = 0;
+   
+
+    switch (cargo){
         case 'diretor':
             percentual = 10;
+            salarioFinal = salario + (salario*0.1);
             break;
         case 'secretario':
             percentual = 20;
+            salarioFinal = salario + (salario*0.2);
             break;
         case 'operador':
             percentual = 30;
+            salarioFinal = salario + (salario*0.3);
             break;
-        default:
-            percentual = 0;
+            
     }
-
-    salarioFinal = salario + (salario * (percentual / 100));
-
-    document.getElementById('resultado').innerHTML = `
-        <p><strong>Salário Atualizado:</strong> R$ ${salarioFinal.toFixed(2)}</p>
-        <p><strong>Percentual Aplicado:</strong> ${percentual}%</p>`;
+    resultado.innerHTML = `Salario com reajuste: ${salarioFinal}<br>
+    porcentagem de reajuste:${percentual}%`
 }
